8385feb6271742452cb0e23d03605438d4e4bef4
Manish Kumar <Krmanish1998@gmail.com>
2026-05-25 05:35:30 +0530

    IMPROVED VERSION

--- practice/practice.py
+++ practice/practice.py
@@ -1,5 +1,5 @@
 def speed_loop(speed):
-    for i in range(0, speed+1, 20):
+    for i in range(1, speed+1, 30):
         if i>120:
             print(f"Overspeed warning: {i}")
             i-=10
@@ -7,17 +7,34 @@ def speed_loop(speed):
         else:
             print(f"Normal speed: {i}")
 def fuel_status(fuel):
-        if fuel<50:
-            print(f"Low Fuel {fuel}")
-        else:
-            print(f"Normal fuel level {fuel}")
+        while fuel>0:
+            if fuel<40:
+                print(f"Low fuel: {fuel}")
+            else:
+                print(f"Fuel level Normal: {fuel}")
+            fuel -= 10
+        print("fuel empty")
 def ign_status(ign):
+    ign=ign.upper()
     if ign=="ON":
         print("Ready to drive")
+        return True
     else:
         print("Turn on ign")
-def vehicle_simulator(speed, fuel, ign):
-    speed_loop(speed)
-    fuel_status(fuel)
-    ign_status(ign)
-vehicle_simulator(178,80,"ON")
+        return False
+def door_status(door, speed):
+    door=door.upper()
+    if door=="OPEN" and speed>0:
+        print("Close the door")
+    else:
+        print("Safe Driving")
+#controller function
+def vehicle_simulator(speed, fuel, ign, door):
+    ign_on=ign_status(ign)
+    if ign_on:
+        speed_loop(speed)
+        fuel_status(fuel)
+        door_status(door, speed)
+    else:
+        print("No vehicle simulation")
+vehicle_simulator(178,30,"on","OpEn")
